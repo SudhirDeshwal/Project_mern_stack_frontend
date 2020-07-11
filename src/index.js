ee1326ee099ff824671cdc6f058b96c8e8ad097e
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-07-11 02:42:54 -0400

    home added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
+import Routes from './user/Routes';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+//ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<Routes />,document.getElementById('root'));
 
 
--- src/user/Routes.js
+++ src/user/Routes.js
@@ -2,19 +2,22 @@ import React from 'react';
 import{BrowserRouter, Switch, Route} from 'react-router-dom'
 import Signin from './Signin'
 import Signup from './Signup'
+import Home from '../core/Home'
+
 
 
 const Routes = () => {
  
     return (
 
-    <div>
+    <BrowserRouter>
        <Switch>
           <Route path="/Signin" exact component={Signin}></Route>
           <Route path="/Signup" exact component={Signup}></Route>
+          <Route path="/" exact component={Home}></Route>
 
        </Switch>
-    </div>
+       </BrowserRouter>
 
     )
 
