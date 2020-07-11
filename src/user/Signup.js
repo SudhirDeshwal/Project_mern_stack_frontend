860516c882a80b649e0c00c1679018b4f3475ef8
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-07-11 04:24:25 -0400

    sign up from done

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import Layout from '../core/Layout'
+//import { API } from '../config';
 
-const Singup = () => (
+const Singup = () => {
 
-<Layout title="Sign up here" discription="Create an Account">
-.....
+      //functions
+
+      const Signupform = () => (
+           
+        <form>
+
+            <div className="form-group">
+            <label className="text-muted">Name</label>
+            <input type="text" className="form-control"></input>
+            </div>
+
+            <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input type="email" className="form-control"></input>
+            </div>
+
+            <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input type="password" className="form-control"></input>
+            </div>
+             
+             <button className="btn-primary">
+                   Submit
+             </button>
+        
+
+        </form>
+
+      )
+
+
+
+    
+    return (
+
+<Layout 
+title="Sign up here" 
+discription="Create an Account"
+className="container col-md-8 offset-md-2">
+ {Signupform()}
 </Layout>
-);
+
+    );
+};
 
 
 
