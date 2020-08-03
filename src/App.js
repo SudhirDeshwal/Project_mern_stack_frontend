91f73017a982ea41cbbe25f7dd4744760a3da76a
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-02 20:11:37 -0400

    category added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PrivateRoute from '../src/auth/PrivateRoute'
 import Dashboard from '../src/user/UserDashboard'
 import AdminRoute from '../src/auth/AdminRoute'
 import AdminDasboard from '../src/user/AdminDasboard'
+import AddCategory from '../src/admin/AddCategory'
 
 
 
@@ -16,11 +17,12 @@ const App = () => {
 
     <BrowserRouter>  
    <Switch>
-          <Route path="/Signin" exact component={Signin}></Route>
+          <Route path="/Signin" exact component={Signin}></Route> 
           <Route path="/Signup" exact component={Signup}></Route>
           <Route path="/" exact component={Home}></Route>
           <PrivateRoute path="/user/dashboard" exact component={Dashboard}></PrivateRoute>
           <AdminRoute path="/admin/dashboard" exact component={AdminDasboard}></AdminRoute>
+          <AdminRoute path="/create/category" exact component={AddCategory}></AdminRoute>
        </Switch>
        </BrowserRouter>
 
