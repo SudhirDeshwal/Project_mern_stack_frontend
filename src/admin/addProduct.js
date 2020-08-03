5c5805717a9c02df15232753191b3bafa6e4eae2
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-02 20:47:03 -0400

    api call to create product

---
+++ src/admin/addProduct.js
@@ -0,0 +1,5 @@
+import React, { useState , useEffect } from 'react';
+import Layout from '../core/Layout'
+import "../styles.css"; 
+import { isAuthenticated } from '../auth/UserAuth'
+import { Link } from "react-router-dom";
