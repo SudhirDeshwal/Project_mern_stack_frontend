3274674fb1c8646953ff0ce60a06c2740832d178
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-03 03:11:57 -0400

    home functions created

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout'
+import {getProducts} from './CoreApicalls'
 
-const Home = () => (
+const Home = () => {
+
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productsByArrival, setProductsByArrival] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsBySell(data);
+            }
+        });
+    };
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            console.log(data);
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsByArrival(data);
+            }
+        });
+    };
+    
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, []);
+
+return (
 
 <Layout title="Explore Books" discription="Buy your Fav books here!!!">
 Books here
 </Layout>
 );
 
-export default Home;
+}
+export default Home; 
