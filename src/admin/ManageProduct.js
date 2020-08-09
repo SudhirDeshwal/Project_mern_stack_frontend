e4b5898e0ace5e02f0c743f4f02bd3d3520e1e8d
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-09 07:16:33 -0400

    list product for admin and delet is done

--- src/admin/ManageProduct.js
+++ src/admin/ManageProduct.js
@@ -8,6 +8,34 @@ import {getProducts,deleteProduct} from './adminApiCalls'
 
 
 const ManageProducts = () => { 
+
+    const [products, setProducts] = useState([]);
+
+    const { user, token } = isAuthenticated(); 
+
+    const loadProducts = () => {
+        getProducts().then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setProducts(data);
+            }
+        });
+    };
+
+    const destroy = productId => {
+        deleteProduct(productId, user._id, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                loadProducts();
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadProducts();
+    }, []);
     
 
     return (
@@ -16,9 +44,36 @@ const ManageProducts = () => {
             discription="Perform CRUD on products"
             className="container-fluid">
 
-                <div>
-                    ......
+<div className="row">
+                <div className="col-12">
+                    <h2 className="text-center">
+                        Total {products.length} products
+                    </h2>
+                    <hr />
+                    <ul className="list-group">
+                        {products.map((p, i) => (
+                            <li
+                                key={i}
+                                className="list-group-item d-flex justify-content-between align-items-center"
+                            >
+                                <strong>{p.name}</strong>
+                                <Link to={`/admin/product/update/${p._id}`}>
+                                    <span className="badge badge-warning badge-pill">
+                                        Update
+                                    </span>
+                                </Link>
+                                <span
+                                    onClick={() => destroy(p._id)}
+                                    className="badge badge-danger badge-pill"
+                                >
+                                    Delete
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                    <br />
                 </div>
+            </div>
             
         </Layout>
     );
