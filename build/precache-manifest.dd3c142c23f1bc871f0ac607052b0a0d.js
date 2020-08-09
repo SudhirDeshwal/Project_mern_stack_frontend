a2b5734a6ab033129126d790ee80420848bf0410
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-09 16:16:16 -0400

    build added

---
+++ build/precache-manifest.dd3c142c23f1bc871f0ac607052b0a0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c99c7f0f876cd0c05d3dc69e5ea1e82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d9e8ce64f70b9f816f",
+    "url": "/static/css/main.a92de0b3.chunk.css"
+  },
+  {
+    "revision": "65cef8512855a852f45f",
+    "url": "/static/js/2.919b19fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.919b19fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d9e8ce64f70b9f816f",
+    "url": "/static/js/main.1b1fb486.chunk.js"
+  },
+  {
+    "revision": "04286168f5215bf69497",
+    "url": "/static/js/runtime-main.9a8d5afe.js"
+  },
+  {
+    "revision": "3ca809b4e8beb9159794f0adbeb7fdf6",
+    "url": "/static/media/header5.3ca809b4.jpg"
+  }
+]);
