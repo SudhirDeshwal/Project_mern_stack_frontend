a2137e7dd81769b1149aef25a533ca89333e1663
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-09 14:57:47 -0400

    changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3001;
 app.use(express.static(publicPath));
+app.use(express.static(__dirname + '/build'));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
