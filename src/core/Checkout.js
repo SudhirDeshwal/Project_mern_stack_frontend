2a28493f30ee5fd321052729ab2a67d331103f96
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-09 01:18:28 -0400

    drop called from brain tree api

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,11 +1,73 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from './Layout';
-import { getCart , removeItem} from './Carthelpers';
+import {getBraintreeClientToken} from './CoreApicalls';
 import Card from './Card';
 import {isAuthenticated} from '../auth/UserAuth'
+import "braintree-web";
+import DropIn from 'braintree-web-drop-in-react';
 
 const Checkout = ({products}) => {
+     
+    const [data, setData] = useState({
+        loading: false,
+        success: false,
+        clientToken: null,
+        error: '',
+        instance: {}
+        
+    });
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
+
+    const getToken = (userId, token) => {
+        getBraintreeClientToken(userId, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+                setData({ ...data, error: data.error });
+            } else {
+                console.log(data);
+                setData({ clientToken: data.clientToken });
+            }
+        });
+    };
+
+
+    const showDropIn = () => (
+
+        
+        <div>
+            {data.clientToken !== null && products.length > 0 ? (
+                <div>
+                    <DropIn
+                        options={{
+                            authorization: data.clientToken,
+                            // paypal: {
+                            //     flow: 'vault'
+                            // }
+                        }}
+                        onInstance={instance => (data.instance = instance)}
+                    />
+                    <button className="btn btn-success btn-block">
+                        Pay
+                    </button>
+
+                    <div class="alert alert-primary" role="alert">
+                                Thank you for showing your interest in Buying our Products.
+                      </div>
+                      <div class="alert alert-warning" role="alert">
+                      Unfortunately ,We are not accepting any payments as of now.
+                       </div>
+                </div>
+            ) : null}
+        </div>
+    );
+
+    useEffect(() => {
+        getToken(userId, token);
+    }, []);
+
 
     const getTotal = () => {
         return products.reduce((currentValue, nextValue) => {
@@ -17,7 +79,7 @@ const Checkout = ({products}) => {
 
         return isAuthenticated() ? (
             
-            <button className="btn btn-success">Proceed to Payments</button>
+        <div>{showDropIn()}</div> 
         
 
         ) : (
